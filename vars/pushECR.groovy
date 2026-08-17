8f52fa7fbb76e92b4fbeb2a4b073dfce467ceb06
naveenkumarg3729-coder <naveenkumarg3729@gmail.com>
2026-08-17 15:28:29 +0530

    Update pushECR.groovy

--- vars/pushECR.groovy
+++ vars/pushECR.groovy
@@ -1,1 +1,14 @@
+def call(String imageName, String accountId, String region) {
 
+    sh """
+    aws ecr get-login-password --region ${region} | \
+    docker login --username AWS --password-stdin \
+    ${accountId}.dkr.ecr.${region}.amazonaws.com
+
+    docker tag ${imageName}:latest \
+    ${accountId}.dkr.ecr.${region}.amazonaws.com/${imageName}:latest
+
+    docker push \
+    ${accountId}.dkr.ecr.${region}.amazonaws.com/${imageName}:latest
+    """
+}
