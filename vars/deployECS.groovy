62ab5f01636c33137fd106e584baa4e7bfa6f8fa
naveenkumarg3729-coder <naveenkumarg3729@gmail.com>
2026-08-17 15:29:09 +0530

    Update deployECS.groovy

--- vars/deployECS.groovy
+++ vars/deployECS.groovy
@@ -1,5 +1,14 @@
-def call(String clusterName, String serviceName) {
-    echo "Deploying to ECS"
-    echo "Cluster: ${clusterName}"
-    echo "Service: ${serviceName}"
+def call(String imageName, String accountId, String region) {
+
+    sh """
+    aws ecr get-login-password --region ${region} | \
+    docker login --username AWS --password-stdin \
+    ${accountId}.dkr.ecr.${region}.amazonaws.com
+
+    docker tag ${imageName}:latest \
+    ${accountId}.dkr.ecr.${region}.amazonaws.com/${imageName}:latest
+
+    docker push \
+    ${accountId}.dkr.ecr.${region}.amazonaws.com/${imageName}:latest
+    """
 }
