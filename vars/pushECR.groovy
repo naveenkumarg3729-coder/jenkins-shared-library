3f0eda92a17d891492e39d7d336903924821107d
naveenkumarg3729-coder <naveenkumarg3729@gmail.com>
2026-08-17 15:41:30 +0530

    Update pushECR.groovy

--- vars/pushECR.groovy
+++ vars/pushECR.groovy
@@ -1,13 +1,9 @@
 def call() {
     sh '''
-    aws ecr get-login-password --region ap-south-2 | \
-    docker login --username AWS --password-stdin \
-    641134884964.dkr.ecr.ap-south-2.amazonaws.com
+    aws ecr get-login-password --region ap-south-2 | docker login --username AWS --password-stdin 641134884964.dkr.ecr.ap-south-2.amazonaws.com
 
-    docker tag bankapp:latest \
-    641134884964.dkr.ecr.ap-south-2.amazonaws.com/bankapp:latest
+    docker tag bankapp:latest 641134884964.dkr.ecr.ap-south-2.amazonaws.com/bankapp:latest
 
-    docker push \
-    641134884964.dkr.ecr.ap-south-2.amazonaws.com/bankapp:latest
+    docker push 641134884964.dkr.ecr.ap-south-2.amazonaws.com/bankapp:latest
     '''
 }
